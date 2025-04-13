d03794185596f5768b22e4fc4a376b657d5b9d31
Bartlomiej-Wieloch <bartlomiej.wieloch@microsoft.wsei.edu.pl>
2025-04-13 12:58:45 +0200

    Complete step 4

--- PudelkoApp/Program.cs
+++ PudelkoApp/Program.cs
@@ -1,5 +1,11 @@
 ﻿using PudelkoLib;
+using System.Globalization;
 
 
-var pudelko = new Pudelko(1, 22, 0, Pudelko.UnitOfMeasure.centimeter);
-Console.WriteLine("Hello, World!");
+var pudelko = new Pudelko(2.5, 9.321, 0.1);
+
+Console.WriteLine("0. " + pudelko.ToString());
+Console.WriteLine("1. " + pudelko.ToString("m"));
+Console.WriteLine("2. " + pudelko.ToString("cm"));
+Console.WriteLine("3. " + pudelko.ToString("mm"));
+
--- PudelkoLib/Pudelko.cs
+++ PudelkoLib/Pudelko.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Threading;
 using System.Threading.Channels;
 using static PudelkoLib.Pudelko.UnitOfMeasure;
 
 namespace PudelkoLib
 {
-    public sealed class Pudelko
+    public sealed class Pudelko : IFormattable
     {
+        CultureInfo cInfo = new CultureInfo("en-US");
+
         private readonly double _a, _b, _c;
         private readonly UnitOfMeasure _unit;
 
         public enum UnitOfMeasure
         {
-            meter, //ustaw format na ... 0.000 -- dla metrów liczą się 3 miejsca po przecinku
+            meter,      //ustaw format na ... 0.000 -- dla metrów liczą się 3 miejsca po przecinku
             centimeter, //ustaw format na ... 000.0 -- dla centymertów liczy się tylko 1 miejsce po przecinku
-            milimeter //ustaw format na ... 0000 -- dla milimetrów nie liczą się miejsca po przecinku
+            milimeter   //ustaw format na ... 0000 -- dla milimetrów nie liczą się miejsca po przecinku
         }
 
         private static double ConvertToMeters(double value, UnitOfMeasure unit)
@@ -22,11 +26,11 @@ namespace PudelkoLib
             if (value <= 0)
                 throw new ArgumentOutOfRangeException("Bok pudełka musi być wartością dodatnią");
 
-            if ((value <= 0.1) && unit == milimeter)
-                throw new ArgumentOutOfRangeException("Bok pudełka musi być większy od 0.1 milimetra");
+            if ((value <= 0.9) && unit == milimeter)
+                throw new ArgumentOutOfRangeException("Bok pudełka musi być większy od 0.9 milimetra");
 
-            if ((value <= 0.01) && unit == centimeter)
-                throw new ArgumentOutOfRangeException("Bok pudełka musi być większy od 0.01 centymetra");
+            if ((value <= 0.09) && unit == centimeter)
+                throw new ArgumentOutOfRangeException("Bok pudełka musi być większy od 0.09 centymetra");
 
             switch (unit)
             {
@@ -37,9 +41,9 @@ namespace PudelkoLib
                 case milimeter:
                     return value / 1000;
                 default:
-                    throw new ArgumentOutOfRangeException("Błędna jednostka");
+                    throw new FormatException("Błędna jednostka");
                     //Nie da się ustawić innej jednostki niż zdefiniowane w UnitOfMeasure,
-                    //ale bez tego jest błąd metody, "not all path return a value
+                    //ale bez tego jest błąd metody, "not all path return a value"
             }
         }
 
@@ -57,9 +61,6 @@ namespace PudelkoLib
             if ((aM > 10) || (bM > 10) || (cM > 10))
                 throw new ArgumentOutOfRangeException("Bok pudełka musi być mniejszy od 10 metrów");
 
-
-
-
             _a = aM;
             _b = bM;
             _c = cM;
@@ -72,6 +73,45 @@ namespace PudelkoLib
         public double C => _c;
         public UnitOfMeasure Unit => _unit;
 
+        public override string ToString()
+        {
+            return ToString("m", null);
+        }
 
+        //public string ToString(string format)
+        //{
+        //    return ToString(format, cInfo);
+        //}
+
+        public string ToString(string format, IFormatProvider? provider = null)
+        {
+            if (String.IsNullOrEmpty(format)) format = "m";
+
+            if (provider == null)
+            {
+                provider = cInfo;
+            }
+
+            switch (format.ToLowerInvariant())
+            {
+                case "m":
+                    return string.Format(provider, "{0:F3} m × {1:F3} m × {2:F3} m", A, B, C);
+
+                case "cm":
+                    var cmA = A * 100;
+                    var cmB = B * 100;
+                    var cmC = C * 100;
+                    return string.Format(provider, "{0:F1} cm × {1:F1} cm × {2:F1} cm", cmA, cmB, cmC);
+
+                case "mm":
+                    var mmA = A * 1000;
+                    var mmB = B * 1000;
+                    var mmC = C * 1000;
+                    return string.Format(provider, "{0:F0} mm × {1:F0} mm × {2:F0} mm", mmA, mmB, mmC);
+
+                default:
+                    throw new FormatException("Błędna jednostka");
+            }
+        }
     }
 }
